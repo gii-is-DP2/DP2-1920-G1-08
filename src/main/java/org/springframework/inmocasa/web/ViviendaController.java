47db7a21287c21e317f0f82f3f4cc9d13e4119bc
AlvaroEsteban <alvaroestebancastrillo@gmail.com>
2020-03-19 11:06:50 +0100

    Terminada Issue #11 Creación del listado de viviendas ordenadas por las más nuevas.

--- src/main/java/org/springframework/inmocasa/web/ViviendaController.java
+++ src/main/java/org/springframework/inmocasa/web/ViviendaController.java
@@ -1,16 +1,39 @@
 package org.springframework.inmocasa.web;
 
-import org.springframework.stereotype.Controller;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.inmocasa.model.Vivienda;
+import org.springframework.inmocasa.service.ViviendaService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping(value="viviendas")
 @Controller
 public class ViviendaController {
 
+	private final ViviendaService vivService;
+	
+	@Autowired
+	public ViviendaController (ViviendaService vivService) {
+		this.vivService = vivService;
+	}
 	
 	//Santi-Alvaro
 	
 	
 	//Alvaro-MiguelEmmanuel
-	
+	@GetMapping(value = {"/allNew"})
+	public String showListViviendas(Map<String, Object> model) {
+		
+		Collection<Vivienda> vivs = vivService.findAllNewest();
+		model.put("viviendas", vivs);
+		
+		return "viviendas/listNewViviendas";
+	}
 	
 	//Alba-Alejandro
 	

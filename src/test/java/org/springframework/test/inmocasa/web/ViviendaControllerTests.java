7180581078df8349f0d823db69c5ae53f198efc4
Miguel <Miguel@192.168.1.198>
2020-05-10 16:34:55 +0200

    Arreglos de Merge

--- src/test/java/org/springframework/test/inmocasa/web/ViviendaControllerTests.java
+++ src/test/java/org/springframework/test/inmocasa/web/ViviendaControllerTests.java
@@ -18,6 +18,7 @@ import static org.hamcrest.Matchers.hasProperty;
 import static org.hamcrest.Matchers.is;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;

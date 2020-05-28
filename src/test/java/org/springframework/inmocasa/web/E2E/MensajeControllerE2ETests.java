36924ccd64f4c485308ffbeffd01dcacf0daa4ba
Alvaro Esteban Castrillo <alvarorota2011@gmail.com>
2020-05-28 14:20:58 +0200

    Santiago alvaro tests (#61)
    * Solución del login en los accesos UI.
    
    * Asserts en casi todas las UI.
    
    * fixed bug edit Vivienda
    
    * Asserts anadidos y nuevas UI completadas.
    
    * Intento de pruebas del model.
    
    * Test model de cliente e intento de reparación los tests E2E
    
    * Service con imports bien
    
    * Pruebas de modelo y servicio de Cliente, Propietario y Vivienda
    
    * correción en data.sql
    
    * Correción test y modelo
    
    Co-authored-by: Santiago <santimartin3105@gmail.com>
    Co-authored-by: alvdiaasc97 <alvdiaasc@alum.us.es>

--- src/test/java/org/springframework/inmocasa/web/E2E/MensajeControllerE2ETests.java
+++ src/test/java/org/springframework/inmocasa/web/E2E/MensajeControllerE2ETests.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.inmocasa.InmocasaApplication;
 import org.springframework.inmocasa.model.Cliente;
 import org.springframework.inmocasa.model.Propietario;
@@ -21,6 +22,10 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import com.paypal.base.rest.APIContext;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
@@ -31,37 +36,32 @@ public class MensajeControllerE2ETests {
 
 	private static int TEST_PROPIETARIO_ID = 1;
 
+	@MockBean
+	APIContext apiContext;
+
 	@Autowired
 	private PropietarioService propietarioService;
 
 	@Autowired
 	private ClienteService clienteService;
 
-
-
 	@Autowired
 	private MockMvc mockMvc;
 
-	@WithMockUser(username = "gilmar", authorities = { "propietario" })
+	@WithMockUser(username = "gilmar", password = "gilmar", authorities = { "propietario" })
 	@Test
 	void testInitCreationForm() throws Exception {
-		mockMvc.perform(get("/mensajes/new")).andExpect(status().isOk()).andExpect(view().name("mensajes/editMensaje"))
-				.andExpect(model().attributeExists("mensaje"));
+		mockMvc.perform(MockMvcRequestBuilders.get("/mensajes/new")).andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(view().name("mensajes/editMensaje"))
+				.andExpect(MockMvcResultMatchers.model().attributeExists("mensaje"));
 	}
 
-	@WithMockUser(username = "gilmar", authorities = { "propietario" })
+	@WithMockUser(username = "admin", authorities = { "admin" })
 	@Test
 	void testProcessCreationFormSuccess() throws Exception {
-		Cliente c = clienteService.findClienteById(TEST_CLIENTE_ID);
-		Propietario p = propietarioService.findPropietarioById(TEST_PROPIETARIO_ID);
+
 		mockMvc.perform(post("/mensajes/save").with(csrf()).param("asunto", "Hola").param("cuerpo", "Amigo"))
 				.andExpect(status().isOk()).andExpect(view().name("/mensajes/editMensaje"));
 	}
-	@WithMockUser(username = "gilmar", authorities = { "propietario" })
-	@Test
-	void testListadoMensajesEnviados() throws Exception {
-		mockMvc.perform(get("/mensajes/mensajes-enviados")).andExpect(view().name("mensajes/misMensajes"))
-				.andExpect(model().attributeExists("mensajes"));
-	}
-//
+
 }
